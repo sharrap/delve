909c828bebf0610377f6bb146a5777efa2e1ddcd
Sean Harrap <sean.harrap@gmail.com>
2020-01-20 19:42:59 -0500

    Add simple form
    Adds a very dumb form with a fake dropdown and a button which updates a
    component.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 
 import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
 import Collapse from '@material-ui/core/Collapse'
+import Container from '@material-ui/core/Container'
+import FormControl from '@material-ui/core/FormControl'
+import Grid from '@material-ui/core/Grid'
+import InputLabel from '@material-ui/core/InputLabel'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
+import MenuItem from '@material-ui/core/MenuItem'
+import Paper from '@material-ui/core/Paper'
+import Select from '@material-ui/core/Select'
 import Typography from '@material-ui/core/Typography'
 
 import BluetoothIcon from '@material-ui/icons/Bluetooth'
@@ -18,14 +26,74 @@ import ExpandMore from '@material-ui/icons/ExpandMore'
 
 const useStyles = makeStyles(theme => ({
   menu: {
-    width: "200px",
+    "min-width": "200px",
     height: "100vh",
     "background-color": "#ddd"
   },
   nested: {
     paddingLeft: theme.spacing(4)
+  },
+  formControl: {
+    width: "120px",
+  },
+  button: {
+    width: "250px",
+  },
+  container: {
+    backgroundColor: "#eee",
+    padding: "20px",
+  },
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "20px",
   }
 }));
+
+// Just pulls data from the server. Simple API test
+function SimpleGenerator(props) {
+  const classes = useStyles();
+
+  const [ value, setValue ] = React.useState("");
+  const [ result, setResult ] = React.useState("");
+  const handleChange = event => { setValue(event.target.value); };
+
+  const getResult = () => { setResult("Example"); }
+
+  return (
+    <Container className={classes.container}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Typography variant="h4">
+              Simple Generator
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <FormControl className={classes.formControl}>
+              <InputLabel id="demo-label-id">Select Table</InputLabel>
+              <Select labelId="demo-label-id" id="test" value={value} onChange={handleChange}>
+                <MenuItem value="a">Table 1</MenuItem>
+                <MenuItem value="b">Table 2</MenuItem>
+              </Select>
+            </FormControl>
+            <Button variant="contained" color="primary" className={classes.button} onClick={getResult}>
+              Get "Random" Result
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} style={{display: result == "" ? "none" : ""}}>
+          <Paper className={classes.paper}>
+            {result}
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
 
 function CollapseList(props) {
   const [ open, setOpen ] = React.useState(true);
@@ -57,6 +125,11 @@ function MenuBar() {
       <List dense component="nav">
         <CollapseList dense descId="menuBar.fantasyText" text="Fantasy Generators">
           <ListItem button className={classes.nested}>
+            <ListItemText>
+              <FormattedMessage id="menuBar.simpleGeneratorText" defaultMessage="Simple Generator" />
+            </ListItemText>
+          </ListItem>
+          <ListItem button className={classes.nested}>
             <ListItemIcon>
               <BluetoothIcon />
             </ListItemIcon>
@@ -79,8 +152,11 @@ function App() {
             <FormattedMessage id="top.titleText" defaultMessage="Delve" />
           </Typography>
         </AppBar>
+        <div style={{display: "flex"}}>
+          <MenuBar />
+          <SimpleGenerator />
+        </div>
       </div>
-      <MenuBar />
     </IntlProvider>
   );
 }
