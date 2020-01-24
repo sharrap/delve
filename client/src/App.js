69d47eb3181e10b5b62eaac89ed20b35e410cadc
Sean Harrap <sean.harrap@gmail.com>
2020-01-23 21:41:15 -0500

    Add mock home and hexcrawl screens
    Adds two mock screens with routing and with a top bar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,78 @@
 import React from 'react';
-import { IntlProvider, FormattedMessage } from 'react-intl'
 import './App.css';
 
-import { AppBar, Typography } from '@material-ui/core'
+import { IntlProvider, FormattedMessage } from 'react-intl';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography
+} from '@material-ui/core';
+
+import {
+  Menu as MenuIcon
+} from '@material-ui/icons';
+
+function Header() {
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" style={{flexGrow: 1}}>
+          <FormattedMessage id="top.titleText" defaultMessage="Delve" />
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+function Home() {
+  return (
+    <Typography variant="h1">
+      <FormattedMessage id="home.text" defaultMessage="Welcome home!" />
+    </Typography>
+  );
+}
+
+function Hexcrawl() {
+  return (
+    <Typography variant="h1">
+      <FormattedMessage id="hexcrawl.text" defaultMessage="Let's crawl!" />
+    </Typography>
+  );
+}
+
+function Body() {
+  return (
+    <Switch>
+      <Route path="/hexcrawl">
+        <Hexcrawl />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  );
+}
 
 function App() {
   return (
     <IntlProvider>
-      <div className="App">
-        <AppBar position="sticky">
-          <Typography variant="h6">
-            <FormattedMessage id="top.titleText" defaultMessage="Delve" />
-          </Typography>
-        </AppBar>
-        <div style={{display: "flex"}}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Body />
         </div>
-      </div>
+      </Router>
     </IntlProvider>
   );
 }
