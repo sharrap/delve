23228a5f337d5a2d046f55ddcc63e8b084454214
Sean Harrap <Sean.Harrap@gmail.com>
2020-02-13 22:02:24 -0500

    Add basic sign in/out menu to user icon

--- client/src/scenes/User/components/AccountButton/index.js
+++ client/src/scenes/User/components/AccountButton/index.js
@@ -2,15 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import { Link as RouterLink } from 'react-router-dom';
+
 import axios from 'axios';
 
 import { actions } from '../../../../_redux';
 
-import { IconButton } from '@material-ui/core';
+import { IconButton, Menu, MenuItem, ListItem } from '@material-ui/core';
 
 import { AccountCircle as AccountIcon } from '@material-ui/icons';
 
 function AccountButton({ authenticated, user, setAuthenticated }) {
+  const buttonRef = React.useRef();
+
+  const [menuOpen, setMenuOpen] = React.useState(false);
+
+  // Prevent menu from re-rendering while closing
+  const [menuAuthenticated, setMenuAuthenticated] = React.useState(false);
+  const [menuClosing, setMenuClosing] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!menuClosing && !menuOpen) {
+      setMenuAuthenticated(authenticated);
+    }
+  }, [authenticated, menuClosing, menuOpen]);
+
   function checkAuthenticated() {
     axios
       .get('/user/logged-in', { withCredentials: true })
@@ -18,12 +34,56 @@ function AccountButton({ authenticated, user, setAuthenticated }) {
       .catch(() => setAuthenticated({ authenticated: false }));
   }
 
+  function tryLogout() {
+    setMenuOpen(false);
+
+    axios
+      .post('/user/signout', { withCredentials: true })
+      .then(() => setAuthenticated({ authenticated: false }))
+      .catch(err => console.log('todo'));
+  }
+
   React.useEffect(checkAuthenticated, []);
 
   return (
-    <IconButton>
-      <AccountIcon />
-    </IconButton>
+    <React.Fragment>
+      <IconButton ref={buttonRef} onClick={() => setMenuOpen(!menuOpen)}>
+        <AccountIcon />
+      </IconButton>
+      <Menu
+        anchorEl={buttonRef.current}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        getContentAnchorEl={null}
+        keepMounted
+        open={menuOpen}
+        onClose={() => setMenuOpen(false)}
+        onExiting={() => setMenuClosing(true)}
+        onExited={() => setMenuClosing(false)}
+      >
+        {!menuAuthenticated && (
+          <MenuItem dense>
+            <ListItem dense component={RouterLink} to="/login">
+              Sign In
+            </ListItem>
+          </MenuItem>
+        )}
+        {!menuAuthenticated && (
+          <MenuItem dense>
+            <ListItem dense component={RouterLink} to="/register">
+              Sign Up
+            </ListItem>
+          </MenuItem>
+        )}
+        {menuAuthenticated && (
+          <MenuItem dense onClick={tryLogout}>
+            Sign Out
+          </MenuItem>
+        )}
+      </Menu>
+    </React.Fragment>
   );
 }
 
